0c0acd9957b9fc82b85286fe62b758ff32a62685
igtz-hub <243434193+igtz-hub@users.noreply.github.com>
2026-04-17 15:58:48 -0400

    Update color definitions for colormaps

--- colormaps.py
+++ colormaps.py
@@ -3,8 +3,8 @@ from matplotlib.colors import LinearSegmentedColormap
 nanoanalysis_bright = LinearSegmentedColormap.from_list('nanoanalysis_bright',['#B50E0E', '#FF8E00', "#F6F93A", "#0080FF", \
                                                                "#452ABE", "#DE46AE"])
 
-nanoanalysis_muted = LinearSegmentedColormap.from_list('nanoanalysis_muted',['#CE0000', '#FF8E00', "#0080FF", \
-                                                               "#BA008B"])
+nanoanalysis_muted = LinearSegmentedColormap.from_list('nanoanalysis_muted',["#BD2020", "#EDC430", "#2FAC55" , \
+                                                               "#2F95AC", "#352FAC", "#B63896"])
 
-nanoanalysis_hazy = LinearSegmentedColormap.from_list('nanoanalysis_hazy',["#DA4F4F", "#8BF371", "#71B2F3", \
-                                                               "#745BE7", "#EA83C9"])
+nanoanalysis_hazy = LinearSegmentedColormap.from_list('nanoanalysis_hazy',["#EA4A4A", "#EFD267", "#5BDD82" , \
+                                                               "#67D5EE", "#5F66E9", "#E484CC"])
