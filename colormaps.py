6d498933ace891b557813ad9120c716f976ffe3e
igtz-hub <243434193+igtz-hub@users.noreply.github.com>
2026-04-17 15:36:10 -0400

    Add custom colormaps for nanoanalysis

---
+++ colormaps.py
@@ -0,0 +1,10 @@
+from matplotlib.colors import LinearSegmentedColormap
+
+nanoanalysis_bright = LinearSegmentedColormap.from_list('nanoanalysis_bright',['#B50E0E', '#FF8E00', "#F6F93A", "#0080FF", \
+                                                               "#452ABE", "#DE46AE"])
+
+nanoanalysis_muted = LinearSegmentedColormap.from_list('nanoanalysis_muted',['#CE0000', '#FF8E00', "#0080FF", \
+                                                               "#BA008B"])
+
+nanoanalysis_hazy = LinearSegmentedColormap.from_list('nanoanalysis_hazy',["#DA4F4F", "#8BF371", "#71B2F3", \
+                                                               "#745BE7", "#EA83C9"])
